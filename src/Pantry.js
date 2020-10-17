34dec35c8720362a07cdb7a8233d3e1a05d883f2
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-17 12:06:22 -0600

    Add check pantry functionality and return shopping list functionality

--- src/Pantry.js
+++ src/Pantry.js
@@ -4,7 +4,38 @@ class Pantry {
     this.recipes = user.recipesToCook;
     this.shoppingList = [];
   }
-}
+  
+  checkPantry(recipe) {
+      let ingredientInRecipe = recipe.mapIngredientsInfo();
+      let isStocked;
+      
+      let pantryIngredients = this.pantry.reduce((inventroy, item) => {
+        return {...inventroy, [item.ingredient] : item.amount};
+      }, {})
+      
+      ingredientInRecipe.forEach(ingredient => {
+        if (!pantryIngredients.hasOwnProperty(ingredient.id)) {
+          isStocked = false;
+          this.shoppingList.push(ingredient)
+        } else if (pantryIngredients[ingredient.id] < ingredient.quantity.amount) {
+          isStocked = false;
+          this.shoppingList.push(ingredient);
+        } else {
+          isStocked = true;
+        }
+      })
+      return isStocked;
+      }
+      
+    returnShoppingList() {
+      
+      return this.shoppingList;
+    }
+  }
+  
+  
+  
+
 
 if (typeof module !== 'undefined') {
   module.exports = Pantry;
