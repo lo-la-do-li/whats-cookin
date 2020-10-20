09b6f567025b7c77112c2db4dc8555c5f81d4f30
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-10-20 15:55:14 -0600

    Add functionality to remove recipies from the favorite

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,8 @@
-// let recipeSet = [];
-
 let modal;
 let trigger;
-let likeBtn
+let likeButton
 let completeRecipeSet
+let removeButton
 //Query Selectors
 let allRecipes = document.querySelector('.all-recipes');
 let recipesDisplay = document.querySelector('.recipes-display');
@@ -11,32 +10,25 @@ let instructions = document.querySelector('.instructions');
 let ingredients = document.querySelector('.ingredients');
 let estimatedCost = document.querySelector('.estimated-cost')
 let closeButton = document.querySelector('.close-button');
-let instructionModal = document.querySelector('.modal')
-let favoritesBtn = document.querySelector('.favorite-recipes-btn')
+let recipeInfoModal = document.querySelector('.modal')
+let favoritesTab = document.querySelector('.tab-favorite-recipes')
 let favoriteDisplay = document.querySelector('.favorite-recipes')
-let current = document.querySelector('.current')
-
-
+let allRecipesTab = document.querySelector('.tab-all-recipes')
 //Functions//Event Listeners
-
 window.addEventListener('click', windowOnClick);
 window.addEventListener('load', onLoad);
 recipesDisplay.addEventListener('click', recipeBlockClickHandler)
 // favoritesView.addEventListener('click', displayFavorites)
-
-
 //Functions
-
 function onLoad() {
   getRandomUser();
   completeRecipeSet = assignRecipes(recipeData)
   displayRecipesAll(completeRecipeSet)
 }
-
 //Call
 function recipeBlockClickHandler(event) {
   if (event.target.classList.contains("close-button")) {
-    instructionModal.classList.toggle("show-modal")
+    recipeInfoModal.classList.toggle("show-modal")
   }
   if (event.target.classList.contains("trigger")) {
     let recipeID = event.target.closest(".recipe-block").id
@@ -46,85 +38,69 @@ function recipeBlockClickHandler(event) {
     let newRecipe = event.target.closest(".recipe-block").id
     addToFavorites(newRecipe)
   }
+  if (event.target.classList.contains("remove-btn")) {
+    let removeRecipe = event.target.closest(".recipe-block").id
+    removeFromFavorites(removeRecipe)
+    displayFavorites()
+  }
 }
-
+//MODAL
 function displayModal(recipeID) {
-  // callModalListeners();
   const foundRecipe = recipeData.find(recipe => {
     return recipe.id === +recipeID
   })
-  document.querySelector('.ingredients').innerHTML = ''
-  document.querySelector('.instructions').innerHTML = ''
-  document.querySelector('.estimated-cost').innerHTML = ''
-  displayRecipeInstructions(foundRecipe, "instructions")
+  document.querySelector('.ingredients').innerText = ''
+  document.querySelector('.instructions').innerText = ''
+  document.querySelector('.estimated-cost').innerText = ''
+  displayRecipeInstructions(foundRecipe, "instructions");
   displayRecipeIngredients(foundRecipe, "ingredients");
-  displayCostOfRecipe(foundRecipe, "estimated-cost")
+  displayCostOfRecipe(foundRecipe, "estimated-cost");
   modal = document.querySelector(".modal");
   modal.classList.toggle("show-modal")
 }
-
-// function callModalListeners() {
-//   modal = document.querySelector(".modal");
-//   trigger = document.querySelector(".trigger");
-//   closeButton = document.querySelector(".close-button");
-//
-//   trigger.addEventListener('click', toggleModal);
-//   closeButton.addEventListener('click', toggleModal);
-// }
-
 function toggleModal(event) {
   modal.classList.toggle("show-modal");
 }
-
+//WINDOW EVENTS
 function windowOnClick(event) {
-  event.preventDefault();
+// event.preventDefault();
   if (event.target === modal) {
     toggleModal();
   }
-  if(event.target === favoritesBtn) {
+  if(event.target === favoritesTab) {
     event.preventDefault();
     displayFavorites()
   }
-  if(event.target === current) {
-    
+  if(event.target === allRecipesTab) {
+    event.preventDefault();
     displayAllView()
   }
     //add other window-clickevent targets Here
-  
 }
-
 function getRandomIndex(arr) {
   return arr[Math.floor(Math.random() * arr.length)]
 }
-
 function getRandomUser() {
   user = new User(getRandomIndex(usersData))
   userPantry = new Pantry(user);
   let message = document.querySelector('.whats-cookin-message');
   message.innerHTML = `What's Cookin' in ${user.name}'s Kitchen?`
 }
-
 // function showElement(className) {
 //   document.querySelector(`.${className}`).remove('hidden')
 // }
-
 function showElement(item) {
   item.classList.remove('hidden')
 }
-
 function hideElement(className) {
   className.classList.add('hidden')
 }
-
 function assignRecipes(recipesArray) {
   return recipesArray.map(recipe => new Recipe(recipe));
 }
-
 function displayRecipesAll(completeRecipeSet) {
+  favoriteDisplay.innerHTML = " ";
   completeRecipeSet.forEach(recipe => {
-// function displayRecipesAll(list) {
-//   list.forEach(recipe => {
-
 const recipeBlock =
 `
   <article class="recipe-block" id="${recipe.id}">
@@ -136,8 +112,8 @@ const recipeBlock =
       <h3 class="tags">${recipe.tags}</h3>
     </hgroup>
     <p>
-      <button class ="like-btn">Like</button>
-      <button>Remove</button>
+      <button class="like-btn">Like</button>
+      <button class="remove-btn">Remove</button>
     </p>
       <button class="trigger" id="trigger">Click here to see more</button>
     </article>
@@ -145,7 +121,6 @@ const recipeBlock =
     allRecipes.insertAdjacentHTML('beforeend', recipeBlock);
     });
   };
-
 function displayRecipeIngredients(recipe, className) {
     recipe.ingredients.forEach(ingredient => {
     ingredient =
@@ -156,25 +131,19 @@ function displayRecipeIngredients(recipe, className) {
     populateModal(ingredient, className);
   });
 }
-
 function displayRecipeInstructions(recipe, className) {
   recipe.instructions.forEach(instruction => {
-
     instruction =
     `${instruction.number}. ${instruction.instruction}</br>`
-
     populateModal(instruction, className);
   });
 }
-
 function populateModal(item, className) {
   document.querySelector(`.${className}`).insertAdjacentHTML('beforeend', item)
 }
-
 // function displayTags(item, className) {
 //   document.querySelector().insertAdjacentHTML('beforeend', item)
 // }
-
 function getIngredientName(ingredient) {
   let name;
   ingredientsData.forEach(ingredientData => {
@@ -184,29 +153,33 @@ function getIngredientName(ingredient) {
   })
   return name;
 }
-
 function displayCostOfRecipe(recipe, className) {
   recipe = new Recipe(recipe)
   let cost = `¢${recipe.getCostOfRecipe()}`
   populateModal(cost, className)
 }
-
 function addToFavorites(newRecipe) {
   const foundRecipe = recipeData.find(recipe => {
      return recipe.id === +newRecipe
   })
     user.addFavoriteRecipes(foundRecipe)
 }
-
+function removeFromFavorites(newRecipe) {
+  let removedRecipe = recipeData.find(recipe => {
+     return recipe.id === +newRecipe
+  })
+    user.removeFavoriteRecipes(removedRecipe)
+}
 
 function displayFavorites() {
   allRecipes.innerHTML = " ";
-  displayFavorited(user.favoritesRecipes);
+  allRecipesTab.classList.remove('current')
+  favoritesTab.classList.add('current')
+  displayFavorited(user.favoriteRecipes);
 }
 
 function displayFavorited(favoriteRecipes) {
   user.favoriteRecipes.forEach(recipe => {
-
 const favoritesBlock =
 `
   <article class="recipe-block" id="${recipe.id}">
@@ -219,7 +192,7 @@ const favoritesBlock =
     </hgroup>
     <p>
       <button class ="like-btn">Like</button>
-      <button>Remove</button>
+      <button class="remove-btn">Remove</button>
     </p>
       <button class="trigger" id="trigger">Click here to see more</button>
     </article>
@@ -227,10 +200,10 @@ const favoritesBlock =
     favoriteDisplay.insertAdjacentHTML('beforeend', favoritesBlock);
     });
   };
-  
   function displayAllView() {
-    favoriteDisplay.innerHTML = " ";
+    favoritesTab.classList.remove('current')
+    allRecipesTab.classList.add('current')
+    completeRecipeSet = assignRecipes(recipeData)
     displayRecipesAll(completeRecipeSet);
-    
   }
 
